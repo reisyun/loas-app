9879fa6a79ce52d218b3946af620395212295360
reisyun <reisyun7@gmail.com>
2021-05-29 22:22:50 +0900

    feat: Add main header widget

---
+++ lib/widget/header/main_header.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MainHeader extends StatelessWidget implements PreferredSizeWidget {
+  final double height = 80.0;
+
+  final String title;
+
+  final List<Widget>? actions;
+
+  MainHeader({required this.title, this.actions});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        title,
+        style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      ),
+      actions: actions,
+      toolbarHeight: height,
+      elevation: 0,
+    );
+  }
+
+  @override
+  get preferredSize => Size.fromHeight(height);
+}
