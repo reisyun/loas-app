5e95364aa7e87018d33c91e624a707547fe9fc4d
reisyun <reisyun7@gmail.com>
2021-06-18 19:20:03 +0900

    fix: Use widgets created

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:storybook_flutter/storybook_flutter.dart';
+import 'package:loas/icon/custom_icons.dart';
+import 'package:loas/widget/header/main_header.dart';
+import 'package:loas/widget/navigation/bottom_nav.dart';
+import 'package:loas/widget/list/library_list.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Storybook(
-        children: [
-          Story.simple(
-            name: 'Input field',
-            child: const TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Input field',
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(color: Color(0xFF2C2F36)),
+        scaffoldBackgroundColor: Color(0xFF2C2F36),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          elevation: 0,
+          backgroundColor: Color(0xFF2C2F36),
+          selectedIconTheme: IconThemeData(color: Color(0xFF8075FF)),
+          unselectedIconTheme: IconThemeData(color: Color(0xFF999999)),
+        ),
+      ),
+      home: Scaffold(
+        appBar: MainHeader(
+          title: '기록',
+          actions: [
+            IconButton(
+              icon: Icon(CustomIcons.search, color: Color(0xFF999999)),
+              onPressed: null,
+            )
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: ListView(children: [
+            LibraryList(
+              title: '감상한 애니',
+              icon: Icon(
+                CustomIcons.history,
+                size: 20,
+                color: Color(0xFF489CEA),
               ),
             ),
-          ),
-          Story(
-            name: 'Raised button',
-            builder: (_, k) => RaisedButton(
-              onPressed:
-                  k.boolean(label: 'Enabled', initial: true) ? () {} : null,
-              color: k.options(
-                label: 'Color',
-                initial: Colors.deepOrange,
-                options: const [
-                  Option('Red', Colors.deepOrange),
-                  Option('Green', Colors.teal),
-                ],
-              ),
-              textColor: Colors.white,
-              child: Text(k.text(label: 'Text', initial: 'Raised button')),
-            ),
-          ),
-        ],
-      );
+          ]),
+        ),
+        bottomNavigationBar: BottomNavigation(),
+      ),
+    );
+  }
 }
